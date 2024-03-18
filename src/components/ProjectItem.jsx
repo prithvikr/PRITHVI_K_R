ceeb5d402a2bfd5245d984c8d64fdf6baea5742c
prithvikr <prithvikr46@gmail.com>
2024-03-19 03:33:46 +0530

    update3

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 
-const ProjectItem = ({img , title, techstack}) => {
+const ProjectItem = ({img , title, techstack, preview , github}) => {
+
+  const handleLivePreviewClick = () => {
+    // Handle live preview button click
+    window.open(preview, '_blank');
+  };
+
+  const handleGithubClick = () => {
+    // Handle github button click
+    window.open(github, '_blank');
+  };
+
   return (
     <div className="bg-slate-600 border border-gray-800 rounded-2xl p-5 flex-1 cursor-pointer hover:bg-gray-500">
     <img 
     src={img} className="w-full h-60" />
     <h3 className="text-2xl font-semibold text-center mt-8">{title}
     </h3>
-    <h4 className="text-gray-400 text-sm mt-2">
+    <h4 className="text-gray-400 font-semibold text-center text-lg mt-2">
       {techstack}
     </h4>
     <div className="flex gap-2 mt-12">
-      <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-600 hover:from-blue-700 hover:to-cyan-700">
+      <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-600 hover:from-blue-700 hover:to-cyan-700"
+        onClick={handleLivePreviewClick}
+      >
         Live preview
       </button>
-      <button className=" flex-1 text-sm py-3  bg-gradient-to-t from-blue-400  to-cyan-300 border rounded-full  hover:border-blue-500 hover:text-blue-500">
+      <button className=" flex-1 text-sm py-3  bg-gradient-to-t from-blue-400  to-cyan-300 border rounded-full  hover:border-blue-500 hover:text-blue-500"
+      onClick={handleGithubClick}
+      >
         Checkout github
       </button>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,59 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
-import amazonImg from '../assets/amazon image.jpg'
-import cynthiaImg from '../assets/cynthia.png'
-import netflixImg from '../assets/netflix.png'
-import portfolioImg from '../assets/portfolio.jpg'
-import spotifyImg from '../assets/spotify.png'
-import todoImg from '../assets/todo.jpg'
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import MERN from "../assets/MERN.png";
+import Amazon from "../assets/amazon.png"
+import netflixImg from "../assets/netflix.png";
+import portfolioImg from "../assets/portfolio.png";
+import todoImg from "../assets/todo.jpg";
 
 const Projects = () => {
   return (
-    <div id='projects' className='max-w-[1040px]  m-auto md:pl-20 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-center uppercase text-[#001b5e]'>Projects</h1>
-        <p className='text-center py-8'></p>
-        <div className='grid sm:grid-cols-2 gap-12'>
-            <ProjectItem img={amazonImg} title='Amazon App' techstack='' />
-            <ProjectItem img={cynthiaImg} title='Cynthia Ugwu' techstack='' /> 
-            <ProjectItem img={netflixImg} title='Netflix App' techstack=''/>
-            <ProjectItem img={portfolioImg} title='Portfolio Me' techstack=''/>
-            <ProjectItem img={spotifyImg} title='Spotify App' techstack=''/>
-            <ProjectItem img={todoImg} title='To Do App' techstack=''/>
-        </div>
-      
+    <div id="projects" className="max-w-[1040px]  m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center uppercase text-[#001b5e]">
+        Projects
+      </h1>
+      <p className="text-center py-8"></p>
+      <div className="grid sm:grid-cols-2 gap-12">
+        
+        <ProjectItem
+          img={Amazon}
+          title="AMAZON"
+          techstack="HTML CSS "
+          preview="https://amazon-pp-chi.vercel.app/"
+          github="https://github.com/prithvikr/AmazonPP"
+        />
+        <ProjectItem
+          img={netflixImg}
+          title="Netflix App"
+          techstack=" HTML CSS VANILLA JS"
+          preview="https://netflix-pp.vercel.app/"
+          github="https://github.com/prithvikr/NetflixPP-Clone"
+        />
+        <ProjectItem
+          img={portfolioImg}
+          title="Portfolio Me"
+          techstack="ReactJS EmailJS React-Router  Bootstrap SASS "
+          preview="prithvi-k-r.vercel.app"
+          github="https://github.com/prithvikr/PRITHVI_K_R"
+        />
+        <ProjectItem
+          img={MERN}
+          title="MERN E-COMMERCE"
+          techstack="NodeJS, ReactJS, MongoDB,Redux, Firebase, SCSS"
+          preview=""
+          github="https://github.com/prithvikr/MERN-E-Commerce"
+        />
+        
+        <ProjectItem
+          img={todoImg}
+          title="To Do App"
+          techstack="HTML CSS VANILLA JS"
+          preview="https://to-do-six-plum.vercel.app/"
+          github="https://github.com/prithvikr/TO_DO"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
